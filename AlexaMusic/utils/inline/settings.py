273f1630c2e3238547d14cc910175c78ebb71410
Dangerop <115329447+Dangerop@users.noreply.github.com>
2022-10-09 07:35:31 +0530

    Update settings.py

--- AlexaMusic/utils/inline/settings.py
+++ AlexaMusic/utils/inline/settings.py
@@ -31,7 +31,7 @@ def setting_markup(_):
         [
             InlineKeyboardButton(
                 text="• Sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/Shayri_Music_Lovers",
+                url="https://t.me/QN_Team_support",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
