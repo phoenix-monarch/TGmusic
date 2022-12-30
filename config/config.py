0f0028b11e07867030beff701cc129dd26f39e0e
Dangerop <115329447+Dangerop@users.noreply.github.com>
2022-12-31 00:01:05 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -105,7 +105,7 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/f6064bc90cc76e59015b2.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/ed1e4016b2209a6d219cb.jpg"
 )
 
 PING_IMG_URL = getenv(
