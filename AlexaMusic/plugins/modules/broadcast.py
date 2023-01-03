0223ed72d7e16f2478dbd0a39509e4e6a9cf90a4
Dangerop <115329447+Dangerop@users.noreply.github.com>
2023-01-04 00:59:33 +0530

    Update broadcast.py

--- AlexaMusic/plugins/modules/broadcast.py
+++ AlexaMusic/plugins/modules/broadcast.py
@@ -73,7 +73,7 @@ async def clean_mode(client, update, users, chats):
     await set_queries(1)
 
 
-@app.on_message(filters.command(BROADCAST_COMMAND) & OWNER_ID)
+@app.on_message(filters.command(BROADCAST_COMMAND) & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
